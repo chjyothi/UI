c514493dcd372969e136e91aa9c6dbaceae6bd29
jyothi <chjyothireddy97@gmail.com>
2020-08-18 00:05:09 -0500

    object destucturing and getter , setter methods &etc

--- javaScript_objects.js
+++ javaScript_objects.js
@@ -145,4 +145,155 @@ const summ = (function() {
 })();
 console.log(summ(1,2,3,4,5));
 
-//spread operator -- 
+//spread operator -- used to hold the values of inplace.
+const ar1 = ['a','b','c'];
+let ar2,ar3;
+(function() {
+    ar2 = ar1;
+    ar3 =[...ar1];
+    ar1[0]='d'
+})();
+console.log(ar2);
+console.log(ar3);
+
+
+/* const key word--It defines a constant reference to a value not,define a const value.
+ --let keyword-- we use for redeclaring the variable
+--reassignning not possible but,we can change the properties of constant object.
+*/
+
+//object destructuring
+var aar = {x: 3, y:7, z:6};
+var x = aar.x;
+var y = aar.y;
+var z = aar.z;
+
+// destructuring
+const {x: a, y: b, z: c} = aar;
+
+const numObj = {
+    present: 10,
+    past : 20
+};
+
+function getPastnum(numObj) {
+const {past : newnum} = numObj;
+return newnum;
+}
+
+console.log(getPastnum(numObj));
+
+//object destructuring inside the object destructuring -- nested objects
+ 
+const forecast = {
+    today : {min: 7, max: 8},
+    tomorrow: {min :6, max: 9}
+};
+
+function getmin(forecat) {
+    const {tomorrow : {min : newmax}}= forecast;
+    return newmax;
+}
+
+console.log(getmin(forecast));
+
+//object as a functions parameter with object destructuring
+//doubt*******************
+const sObj = {
+    even : 2,
+    neg : -1,
+    odd : 3,
+    zero : 0
+};
+const half = (function() {
+    return function half({even, odd}) {
+        return (even + odd) /2.0;
+    };
+})();
+console.log(sObj);
+console.log(half(sObj)); 
+
+//creating strings using Template Literals
+const result = {
+    student1: ["jyothi","89","full time"],
+    student2: ["reddy","90","contractor"],
+    student3: ["friend","85","contractoe"]
+};
+function makeList(arr) {
+const resultDisplay = [];
+for(let i=0; i< arr.length; i++)
+{
+    resultDisplay.push(`<li class="text-warning"${arr[i]}</li>`)
+}
+return resultDisplay;
+}
+const resultDisplay = makeList(result.student2);
+console.log(resultDisplay);
+
+//object literals using array functions
+//Its taking strings and returning object
+const person = (name, age, gender) => ({name, age, gender});
+console.log(person("jyothi","89","female"));
+
+//one way to write function with in a object
+const student = {
+attend: 2,
+AVGattendance(NEWattands) {
+    "use strict";
+    this.attend = NEWattands;
+}
+};
+ 
+student.AVGattendance(4);
+console.log(student.attend);
+/*"use script" -- is a literal expression, indicting that code should be 
+executed in strict mode. It eliminates some sailent errors by chnaging them to throw errors.
+*/
+/*------------------------------------------*****---------*/
+//use class syntax to define constructor function
+class spelling {
+    constructr(targetSpelling){
+        this.targetSpelling = targetSpelling;
+    }
+}
+//object creation
+var spell = new spelling('Jyo Reddy');
+console.log(spell.targetSpelling);
+
+//getter and setter methods to control acesses to an object
+
+function makeClass() {
+    class Thermostat {
+        consructor(temp) {
+            this._temp = temp+5;
+            //'_' represents that varible is private and local
+        }
+       
+        get temparature() {
+            return this._temp;
+        }
+        set temparature(updatestemp) {
+            this._temp=updatestemp;
+
+        }
+    }
+    return Thermostat;
+}
+
+const Thsat = makeClass();
+const thmos = new Thsat(70);
+let temp = thmos.temparature;
+thmos.temparature = 22;
+temp = thmos.temparature;
+console.log(temp);
+
+/*import vs require
+"./xyz" --current directory
+ import --: import { function name} from "./path" 
+-----'* as uname from "./path"' --we can import everything from that file
+export --: export const xname = arg => arg.toupper()    --------(function name)
+export { function name}; ---can import from any file
+
+If we want to export only one thing from file we use "default" --fallback
+If it is default fallback we dont use '{}' while importing
+*/
